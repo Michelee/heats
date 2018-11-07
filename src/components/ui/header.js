daa5f4cc5e23e16bc117c66b3f23dbb548c23c1e
Michele Ancheta <michelee.ancheta@gmail.com>
2018-11-07 12:37:59 -0500

    update responsive

--- src/components/ui/header.js
+++ src/components/ui/header.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-scroll'
 import Logo from '../icons/heats-logo'
+import Hamburguer from '../icons/hamburger'
 import '../../styles/ui/header.scss'
 
 const scrollConfig = {
@@ -9,21 +10,39 @@ const scrollConfig = {
   duration: 1000
 }
 
-const Header = () => (
-  <div className="header-container">
-    <div className="header-content">
-      <Link to="home" {...scrollConfig}>
-        <Logo />
-      </Link>
-      <div className="menu">
-        <Link to="home" {...scrollConfig}>Home</Link>
-        <Link to="application" {...scrollConfig}>Applications</Link>
-        <Link to="product" {...scrollConfig}>Products</Link>
-        <Link to="about" {...scrollConfig}>Our Story</Link>        
-        <Link to="contact" {...scrollConfig}>Contact Us</Link>        
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggle: false
+    }
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+  handleToggle () {
+    const toggle = this.state.toggle
+    this.setState({
+      toggle: !toggle
+    })
+  }
+  render() {
+    return (
+      <div className="header-container">
+        <div className="header-content">
+          <Link to="home" {...scrollConfig}>
+            <Logo />
+          </Link>
+          <div className={`menu ${this.state.toggle ? 'show' : ''}`}>
+            <Link to="home" {...scrollConfig}>Home</Link>
+            <Link to="application" {...scrollConfig}>Applications</Link>
+            <Link to="product" {...scrollConfig}>Products</Link>
+            <Link to="about" {...scrollConfig}>Our Story</Link>        
+            <Link to="contact" {...scrollConfig}>Contact Us</Link> 
+          </div>
+          <a class="icon" onClick={this.handleToggle}><Hamburguer /></a>  
+        </div>
       </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 export default Header
